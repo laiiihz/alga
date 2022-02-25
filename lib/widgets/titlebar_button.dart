dead113eb08d66432588c1eaf3984a6f9f9b7178
laiiihz <laiiihz@outlook.com>
2022-02-25 16:52:45 +0800

    update buttons

--- lib/widgets/titlebar_button.dart
+++ lib/widgets/titlebar_button.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
 class TitlebarButtonGroup extends StatefulWidget {
   const TitlebarButtonGroup({Key? key}) : super(key: key);
@@ -11,7 +12,6 @@ class TitlebarButtonGroup extends StatefulWidget {
 
 class _TitlebarButtonGroupState extends State<TitlebarButtonGroup> {
   bool _showIcons = false;
-  bool _isMac = Platform.isMacOS;
   @override
   Widget build(BuildContext context) {
     List<Widget> children = [
@@ -19,18 +19,28 @@ class _TitlebarButtonGroupState extends State<TitlebarButtonGroup> {
         showIcon: _showIcons,
         child: const Icon(Icons.fullscreen),
         color: const Color(0xFF61C554),
+        onPressed: () async {
+          bool isFull = await windowManager.isFullScreen();
+          await windowManager.setFullScreen(!isFull);
+        },
       ),
       const SizedBox(width: 8),
       TitlebarButton(
         showIcon: _showIcons,
         child: const Icon(Icons.remove),
         color: const Color(0xFFF4BF4F),
+        onPressed: () {
+          windowManager.minimize();
+        },
       ),
       const SizedBox(width: 8),
       TitlebarButton(
         showIcon: _showIcons,
         child: const Icon(Icons.close),
         color: const Color(0xFFEC6A5E),
+        onPressed: () {
+          windowManager.close();
+        },
       ),
     ];
 
@@ -58,11 +68,13 @@ class TitlebarButton extends StatefulWidget {
   final bool showIcon;
   final Widget child;
   final Color color;
+  final VoidCallback onPressed;
   const TitlebarButton(
       {Key? key,
       required this.showIcon,
       required this.child,
-      required this.color})
+      required this.color,
+      required this.onPressed})
       : super(key: key);
 
   @override
@@ -90,7 +102,7 @@ class _TitlebarButtonState extends State<TitlebarButton> {
           child: widget.child,
         ),
       ),
-      onPressed: () {},
+      onPressed: widget.onPressed,
     );
   }
 }
