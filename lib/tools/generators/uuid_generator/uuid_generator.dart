7cff88743255c1d781b0aeb0d74a9bd4bfae3500
laiiihz <laiiihz@outlook.com>
2022-03-18 09:51:18 +0800

    uuid migrate to riverpod

--- lib/tools/generators/uuid_generator/uuid_generator.dart
+++ lib/tools/generators/uuid_generator/uuid_generator.dart
@@ -1,6 +1,12 @@
 import 'package:alga/constants/import_helper.dart';
-import 'package:alga/tools/generators/uuid_generator/uuid_provider.dart';
+import 'package:alga/utils/clipboard_util.dart';
+import 'package:alga/widgets/ref_readonly.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:uuid/uuid.dart';
+
+part './uuid_provider.dart';
 
 class UUIDGeneratorView extends StatefulWidget {
   const UUIDGeneratorView({Key? key}) : super(key: key);
@@ -10,24 +16,6 @@ class UUIDGeneratorView extends StatefulWidget {
 }
 
 class _UUIDGeneratorViewState extends State<UUIDGeneratorView> {
-  final _provider = UUIDProvider();
-  update() {
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _provider.addListener(update);
-  }
-
-  @override
-  void dispose() {
-    _provider.removeListener(update);
-    _provider.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return ToolView.scrollVertical(
@@ -38,91 +26,104 @@ class _UUIDGeneratorViewState extends State<UUIDGeneratorView> {
             ToolViewConfig(
               leading: const Icon(Icons.horizontal_rule),
               title: const Text('Hypens'),
-              trailing: Switch(
-                value: _provider.hypens,
-                onChanged: (value) {
-                  _provider.hypens = value;
-                },
-              ),
+              trailing: Consumer(builder: (context, ref, _) {
+                return Switch(
+                  value: ref.watch(_hypens),
+                  onChanged: (value) {
+                    ref.read(_hypens.notifier).state = value;
+                  },
+                );
+              }),
             ),
             ToolViewConfig(
               leading: const Icon(Icons.text_fields),
               title: Text(S.of(context).upperCase),
-              trailing: Switch(
-                value: _provider.upperCase,
-                onChanged: (value) {
-                  _provider.upperCase = value;
-                },
-              ),
+              trailing: Consumer(builder: (context, ref, _) {
+                return Switch(
+                  value: ref.watch(_upperCase),
+                  onChanged: (value) {
+                    ref.read(_upperCase.notifier).state = value;
+                  },
+                );
+              }),
             ),
             ToolViewConfig(
               leading: const Icon(Icons.info_outline),
               title: const Text('UUID Version'),
               subtitle:
                   const Text('Choose the version of UUID version to generate'),
-              trailing: DropdownButton<UUIDVersion>(
-                items: UUIDVersion.values.map((e) {
-                  return DropdownMenuItem(child: Text(e.value), value: e);
-                }).toList(),
-                value: _provider.version,
-                onChanged: (version) {
-                  _provider.version = version ?? UUIDVersion.v1;
-                },
-              ),
+              trailing: Consumer(builder: (context, ref, _) {
+                return DropdownButton<UUIDVersion>(
+                  items: UUIDVersion.values.map((e) {
+                    return DropdownMenuItem(
+                      child: Text(e.typeName(context)),
+                      value: e,
+                    );
+                  }).toList(),
+                  value: ref.watch(_version),
+                  onChanged: (version) {
+                    ref.read(_version.notifier).state =
+                        version ?? UUIDVersion.v1;
+                  },
+                );
+              }),
             ),
           ],
         ),
         const AppTitle(title: 'generate'),
         Row(
           children: [
-            ElevatedButton(
-              child: const Text('Generate UUIDs'),
-              onPressed: () {
-                _provider.generate();
-              },
-            ),
+            RefReadonly(builder: (ref) {
+              return ElevatedButton(
+                child: const Text('Generate UUIDs'),
+                onPressed: () {
+                  ref.refresh(_results);
+                },
+              );
+            }),
             const Padding(
               padding: EdgeInsets.symmetric(horizontal: 8),
               child: Text('X'),
             ),
             SizedBox(
               width: 100,
-              child: TextField(
-                controller: _provider.numberController,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                onChanged: (value) {
-                  final _value = int.tryParse(value);
-                  _provider.count = _value ?? 1;
-                },
-                decoration: const InputDecoration(
-                  isDense: true,
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-                ),
-              ),
+              child: RefReadonly(builder: (ref) {
+                return TextField(
+                  controller: ref.watch(_countController),
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  onChanged: (value) {
+                    ref.refresh(_count);
+                  },
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                  ),
+                );
+              }),
             )
           ],
         ),
         AppTitle(
           title: 'UUIDs',
           actions: [
-            IconButton(
-              icon: const Icon(Icons.copy),
-              onPressed: () async {
-                await _provider.copy();
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.clear),
-              onPressed: () {},
-            ),
+            RefReadonly(builder: (ref) {
+              return IconButton(
+                icon: const Icon(Icons.copy),
+                onPressed: () async {
+                  ClipboardUtil.copy(ref.read(_resultValue));
+                },
+              );
+            }),
           ],
         ),
-        TextField(
-          minLines: 2,
-          maxLines: 20,
-          controller: _provider.resultController,
-        ),
+        Consumer(builder: (context, ref, _) {
+          return AppTextBox(
+            minLines: 2,
+            maxLines: 20,
+            data: ref.watch(_resultValue),
+          );
+        }),
       ],
     );
   }
--- lib/tools/generators/uuid_generator/uuid_provider.dart
+++ lib/tools/generators/uuid_generator/uuid_provider.dart
@@ -1,7 +1,4 @@
-import 'package:alga/tools/generators/abstract/generator_base.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:uuid/uuid.dart';
+part of './uuid_generator.dart';
 
 enum UUIDVersion {
   v1,
@@ -9,7 +6,7 @@ enum UUIDVersion {
 }
 
 extension UUIDExt on UUIDVersion {
-  String get value {
+  String typeName(BuildContext context) {
     switch (this) {
       case UUIDVersion.v1:
         return '1'.padRight(10);
@@ -19,50 +16,32 @@ extension UUIDExt on UUIDVersion {
   }
 }
 
-class UUIDProvider extends GeneratorBase {
-  final uuid = const Uuid();
-  bool _hypens = true;
-  bool get hypens => _hypens;
-  set hypens(bool state) {
-    _hypens = state;
-    notifyListeners();
-  }
+final _uuid = StateProvider.autoDispose((ref) => const Uuid());
 
-  bool _upperCase = true;
-  bool get upperCase => _upperCase;
-  set upperCase(bool state) {
-    _upperCase = state;
-    notifyListeners();
-  }
+final _version =
+    StateProvider.autoDispose<UUIDVersion>((ref) => UUIDVersion.v4);
+final _hypens = StateProvider.autoDispose<bool>((ref) => true);
+final _upperCase = StateProvider.autoDispose<bool>((ref) => true);
+final _countController =
+    StateProvider.autoDispose<TextEditingController>((ref) {
+  final controller = TextEditingController();
+  ref.onDispose(controller.dispose);
+  return controller;
+});
 
-  UUIDVersion _version = UUIDVersion.v4;
-  UUIDVersion get version => _version;
-  set version(UUIDVersion state) {
-    _version = state;
-    notifyListeners();
-  }
+final _count = StateProvider.autoDispose<int>((ref) {
+  return int.tryParse(ref.read(_countController).text) ?? 1;
+});
 
-  int _count = 1;
-  int get count => _count;
-  set count(int value) {
-    _count = value;
-    notifyListeners();
-  }
-
-  TextEditingController resultController = TextEditingController();
-  TextEditingController numberController = TextEditingController();
-
-  Future copy() async {
-    await Clipboard.setData(ClipboardData(text: resultController.text));
-  }
-
-  @override
-  void clear() {
-    resultController.clear();
-  }
+final _results = StateProvider.autoDispose<List<String>>((ref) {
+  final uuid = ref.read(_uuid);
+  final version = ref.watch(_version);
+  final hypen = ref.watch(_hypens);
+  final uppercase = ref.watch(_upperCase);
+  final count = ref.watch(_count);
 
   String _genId() {
-    switch (_version) {
+    switch (version) {
       case UUIDVersion.v1:
         return uuid.v1();
       case UUIDVersion.v4:
@@ -70,27 +49,15 @@ class UUIDProvider extends GeneratorBase {
     }
   }
 
-  String _decoratedData() {
-    String result = _genId();
-    if (!_hypens) {
-      result = result.replaceAll('-', '');
-    }
-    if (_upperCase) {
-      result = result.toUpperCase();
-    }
-    return result;
+  String _convertValue() {
+    String value = _genId();
+    if (!hypen) value = value.replaceAll('-', '');
+    if (uppercase) value = value.toUpperCase();
+    return value;
   }
 
-  @override
-  generate() {
-    List<String> results = List.generate(_count, (index) => _decoratedData());
-    resultController.text = results.join('\n');
-  }
+  return List.generate(count, (index) => _convertValue());
+});
 
-  @override
-  void dispose() {
-    resultController.dispose();
-    numberController.dispose();
-    super.dispose();
-  }
-}
+final _resultValue =
+    StateProvider.autoDispose<String>((ref) => ref.watch(_results).join('\n'));
