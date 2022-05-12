8e88da5ba84710b7ca89a7e86c32db7b047533e2
laiiihz <laiiihz@outlook.com>
2022-05-12 17:11:57 +0800

    update flutter sdk to 3.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:alga/l10n/l10n.dart';
