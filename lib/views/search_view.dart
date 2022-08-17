e1b062afe109de0efc5f37d9cf68e164453a254f
laiiihz <laiiihz@outlook.com>
2022-08-17 16:36:38 +0800

    update search view

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -42,57 +42,62 @@ class _SearchTileState extends ConsumerState<SearchTile> {
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 32),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Material(
-                borderRadius: BorderRadius.circular(8),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 48,
-                  child: TextField(
-                    controller: _query,
-                    onChanged: (text) {
-                      _toolItems = searchByText(context, text);
-                      setState(() {});
-                    },
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: context.tr.search,
-                      filled: false,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              AnimatedSize(
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeInOutCubic,
-                child: _toolItems.isEmpty
-                    ? const SizedBox.shrink()
-                    : Material(
-                        borderRadius: BorderRadius.circular(8),
-                        child: SizedBox(
-                          height: MediaQuery.of(context).size.height - 256,
-                          child: ListView.builder(
-                            itemBuilder: (context, index) {
-                              final item = _toolItems[index];
-                              return ListTile(
-                                leading: item.icon,
-                                title: Text(item.name(context)),
-                                onTap: () {
-                                  ref.watch(currentWidget.state).state =
-                                      item.widget;
-                                  Navigator.of(context).pop();
-                                },
-                              );
-                            },
-                            itemCount: _toolItems.length,
-                          ),
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 800),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Material(
+                    borderRadius: BorderRadius.circular(8),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 48,
+                      child: TextField(
+                        controller: _query,
+                        onChanged: (text) {
+                          _toolItems = searchByText(context, text);
+                          setState(() {});
+                        },
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: context.tr.search,
+                          filled: false,
                         ),
                       ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  AnimatedSize(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOutCubic,
+                    child: _toolItems.isEmpty
+                        ? const SizedBox.shrink()
+                        : Material(
+                            borderRadius: BorderRadius.circular(8),
+                            child: SizedBox(
+                              height: MediaQuery.of(context).size.height - 256,
+                              child: ListView.builder(
+                                itemBuilder: (context, index) {
+                                  final item = _toolItems[index];
+                                  return ListTile(
+                                    leading: item.icon,
+                                    title: Text(item.name(context)),
+                                    onTap: () {
+                                      ref.watch(currentWidget.state).state =
+                                          item.widget;
+                                      Navigator.of(context).pop();
+                                    },
+                                  );
+                                },
+                                itemCount: _toolItems.length,
+                              ),
+                            ),
+                          ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ],
